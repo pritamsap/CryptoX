3cf34fb98e834b96dff7f15fe4bed817e62e4e37
pritamsap <pritamsap2@gmail.com>
2024-09-17 19:37:52 -0400

    Initial Commit

---
+++ CryptoX/CryptoXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoXApp.swift
+//  CryptoX
+//
+//  Created by pritam on 2024-09-17.
+//
+
+import SwiftUI
+
+@main
+struct CryptoXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
